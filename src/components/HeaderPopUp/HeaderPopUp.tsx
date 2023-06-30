4de0b34a9ac7006e7d2a6a8b5a1bf30328ff24b9
Rasvom <rasvomb@gmail.com>
2023-06-30 17:21:41 +0300

    add pop-up

--- src/components/HeaderPopUp/HeaderPopUp.tsx
+++ src/components/HeaderPopUp/HeaderPopUp.tsx
@@ -1,14 +1,36 @@
+import React, { useEffect, useRef } from "react";
 import styles from "./HeaderPopUp.module.css";
 import user from "../../assets/user.png";
 import "boxicons";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const HeaderPopUp = () => {
-  
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const token = false;
+
+  const onClickPopUp = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !event.composedPath().includes(menuRef.current)) {
+        setIsOpen(false);
+      }
+    };
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.main}>
-      <Link>Сдать в аренду</Link>
-      <div className={styles.block}>
+      <Link to="">Сдать в аренду</Link>
+      <div ref={menuRef} className={styles.block} onClick={onClickPopUp}>
         <div className={styles.hrs}>
           <box-icon name="menu"></box-icon>
         </div>
@@ -16,6 +38,22 @@ const HeaderPopUp = () => {
           <img src={user} alt="" />
         </div>
       </div>
+
+      {token ? (
+        isOpen && (
+          <div className={styles.popUp}>
+            <Link to="/">Профиль</Link>
+            <Link to="/">Что-то</Link>
+          </div>
+        )
+      ) : (
+        isOpen && (
+          <div className={styles.popUp}>
+            <Link to="/login">Войти</Link>
+            <Link to="/auth">Зарегистрироваться</Link>
+          </div>
+        )
+      )}
     </div>
   );
 };
