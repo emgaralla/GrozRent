86b06c8a84b2d74667201e7842c2d200dd639da9
shishanee <mansur72095@gmail.com>
2023-06-30 10:45:41 +0300

    Сделал авторизацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import SignIn from "./components/Sign/SignIn";
+import SignUp from "./components/Sign/SignUp";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route></Route>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/auth" element={<SignUp />}></Route>
       </Routes>
     </div>
   );
