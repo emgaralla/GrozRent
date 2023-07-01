6bd4ff872eb5854e2d0f3437485bb0da48f763be
shishanee <mansur72095@gmail.com>
2023-07-01 17:25:47 +0300

    сделал слайс

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/applicationSlice";
 import categories from "../features/categoriesSlice";
+import products from "../features/products.Slice";
 
 export const store = configureStore({
   reducer: { 
     application,
-    categories
-   },
+    categories,
+    products,
+  },
 });
