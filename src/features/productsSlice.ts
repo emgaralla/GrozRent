d19ea624be4f4fa49ac47accabd713cb0bf6e82e
shishanee <mansur72095@gmail.com>
2023-07-04 11:26:52 +0300

    oneProduct style

--- src/features/productsSlice.ts
+++ src/features/productsSlice.ts
@@ -37,13 +37,13 @@ export const fetchProducts = createAsyncThunk(
 export const createProduct = createAsyncThunk(
   "products/create",
   async ({ title, text, adress, phone, price, image, categorie }, thunkAPI) => {
-    console.log(image);
     try {
       const formData = new FormData();
       formData.append("img", image[0]);
       formData.append("img", image[1]);
       formData.append("img", image[2]);
       formData.append("img", image[3]);
+      formData.append("img", image[4]);
       formData.append("title", title);
       formData.append("text", text);
       formData.append("adress", adress);
